1f2ff80ad75d31478c7538fcfa5d1a5723d546ec
Dallas Read <dallas@excitecreative.ca>
2021-09-18 07:40:14 -0300

    CSS

--- src/components/Login/component.vue
+++ src/components/Login/component.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="with-mobile-padding">
     <h4>Connect your domain to Heroku.</h4>
     <a aria-label="Connect via DNSimple" class="button" :href="oauthUrl">Connect via DNSimple</a>
   </div>
