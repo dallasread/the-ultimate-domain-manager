3c59cdafd145e1a791aaeea7d964ee2ac985ccab
Dallas Read <dallas@excitecreative.ca>
2021-10-17 14:04:22 -0300

    Fix icons?

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/the-ultimate-domain-manager/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/the-ultimate-domain-manager/' : '/',
+  pwa: {
+    themeColor: '#FFCF2D',
+    msTileColor: '#FFCF2D',
+    iconPaths: {
+      msTileImage: 'img/icons/mstile-150x150.png'
+    }
+  }
 }
