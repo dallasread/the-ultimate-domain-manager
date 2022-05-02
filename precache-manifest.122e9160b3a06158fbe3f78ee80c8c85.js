7794a76cdfa66557decd70a133d41cbc02f79856
Dallas Read <dallas@excitecreative.ca>
2022-05-02 10:49:14 -0300

    gh-pages

---
+++ precache-manifest.122e9160b3a06158fbe3f78ee80c8c85.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79fb4c4151b373f9adc014a13dcf57ee",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "09245a49ddda6b98c25d",
+    "url": "/css/app.489407ed.css"
+  },
+  {
+    "revision": "f283a3b250a9f23e2122d78cc06f3779",
+    "url": "/img/logo.f283a3b2.svg"
+  },
+  {
+    "revision": "6edc5c484d56702d6093d2e3cd7df0c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09245a49ddda6b98c25d",
+    "url": "/js/app.0b21c45e.js"
+  },
+  {
+    "revision": "8b89e72b020ae85c29cb",
+    "url": "/js/chunk-vendors.5ed29cae.js"
+  },
+  {
+    "revision": "5d2a4346bbe771e14fabb92f312a30c8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
